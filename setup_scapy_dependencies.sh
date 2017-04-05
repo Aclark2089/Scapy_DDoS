c95e534b6ef1c9ba401b6ae1c0121826026194e1
R. Alex Clark <aclark2089@gmail.com>
2017-04-05 12:06:53 -0400

    Setup script for getting dependencies

---
+++ setup_scapy_dependencies.sh
@@ -0,0 +1,3 @@
+sudo apt-get install libpcap0.8-dev python-libpcap
+sudo apt-get install libdnet-dev python-dumbnet
+sudo apt-get install tcpdump graphviz imagemagick python-gnuplot python-crypto python-pyx
